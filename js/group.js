051a422c3d9db729fc48b3d104c6404b3f7a83a4
Pedro Carrazco <fzn.webdesign@gmail.com>
2014-09-17 10:41:34 -0500

    Architecture Updated, bugs fixed and StrictMode

--- js/group.js
+++ js/group.js
@@ -3,10 +3,28 @@ var webApp = webApp || {};
 webApp.Group = function (){
 	this.items = {};
 }
-webApp.Group.prototype = new webApp.Drawable();
+webApp.Group.prototype = new webApp.Object();
 webApp.Group.prototype.extend({
 	Group: function(items,p,parent){
-		this.Drawable(p,parent);
+		var params = p || {};
+		var parent   = parent || false;
+
+		// Basic data
+		this.canvas   = params.canvas || this.canvas;
+		this.name     = params.name || this.name || "item" + Math.random();
+		this.UID      = params.UID || "INVALID";
+		this.type     = params.type || false;
+		this.setParent(parent);
+
+		// Drawing data
+		this.pos      = this.validateCoords(params.pos);
+		this.size     = this.validateCoords(params.size,[10,10]);
+		this.opacity  = (typeof params.opacity != "undefined") ? params.opacity : 1;
+		this.color    = params.color || false;
+		this.image    = params.image || false;
+		this.imagePos = this.validateCoords(params.imagePos);
+		this.tile     = params.tile || false;
+		
 		this.items = {};
 		
 		for(var itm in items){
